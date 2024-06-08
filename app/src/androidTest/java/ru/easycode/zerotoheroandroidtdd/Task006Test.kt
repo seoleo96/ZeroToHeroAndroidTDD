65ad3019c039827427957e42bb3b691edc3aa69e
sardoriiiy <s.sarimsakov@indev.expert>
2024-06-08 12:43:01 +0500

    done 6 test

--- app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
+++ app/src/androidTest/java/ru/easycode/zerotoheroandroidtdd/Task006Test.kt
@@ -7,11 +7,14 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.PositionAssertions
 import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.espresso.matcher.ViewMatchers.isAssignableFrom
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withParent
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.hamcrest.Matchers.allOf
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
