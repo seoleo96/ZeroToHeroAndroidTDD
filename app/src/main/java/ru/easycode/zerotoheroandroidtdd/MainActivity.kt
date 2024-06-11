e8d7ac933fc2364f59a326f5f711f5167c54d652
sardoriiiy <s.sarimsakov@indev.expert>
2024-06-11 19:22:58 +0500

    done 9 test

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,41 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var rootLayout: LinearLayout
+    private lateinit var titleTextView: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val removeButton = findViewById<Button>(R.id.removeButton)
+        titleTextView = findViewById<TextView>(R.id.titleTextView)
+        rootLayout = findViewById<LinearLayout>(R.id.rootLayout)
+        removeButton.setOnClickListener {
+            rootLayout.removeView(titleTextView)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val isNeedRemove = rootLayout.childCount == 1
+        outState.putBoolean(REMOVE_TEXT_VIEW_KEY, isNeedRemove)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val isNeedRemove = savedInstanceState.getBoolean(REMOVE_TEXT_VIEW_KEY)
+        if (isNeedRemove)
+            rootLayout.removeView(titleTextView)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
+        const val REMOVE_TEXT_VIEW_KEY = "removeTextViewKey"
     }
 }
