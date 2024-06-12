1ed977c901719c66dc92f2b39e2c5d561b261149
sardoriiiy <s.sarimsakov@indev.expert>
2024-06-12 22:03:33 +0500

    without if/else

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,14 +1,18 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.os.Build
 import android.os.Bundle
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.io.Serializable
 
 class MainActivity : AppCompatActivity() {
     private lateinit var rootLayout: LinearLayout
     private lateinit var titleTextView: TextView
+    private var state: State = State.Initial
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,25 +21,42 @@ class MainActivity : AppCompatActivity() {
         titleTextView = findViewById<TextView>(R.id.titleTextView)
         rootLayout = findViewById<LinearLayout>(R.id.rootLayout)
         removeButton.setOnClickListener {
-            rootLayout.removeView(titleTextView)
+            state = State.Removed
+            state.apply(linearLayout = rootLayout, textView = titleTextView)
         }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
-        val isNeedRemove = rootLayout.childCount == 1
-        outState.putBoolean(REMOVE_TEXT_VIEW_KEY, isNeedRemove)
+        outState.putSerializable(REMOVE_TEXT_VIEW_KEY, state)
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
-        val isNeedRemove = savedInstanceState.getBoolean(REMOVE_TEXT_VIEW_KEY)
-        if (isNeedRemove)
-            rootLayout.removeView(titleTextView)
+        state = savedInstanceState.getSerializable(REMOVE_TEXT_VIEW_KEY, State::class.java) as State
+        state.apply(linearLayout = rootLayout, textView = titleTextView)
     }
 
     companion object {
         private const val TAG = "MainActivity"
         const val REMOVE_TEXT_VIEW_KEY = "removeTextViewKey"
+    }
+}
+
+interface State : Serializable {
+
+    fun apply(linearLayout: LinearLayout, textView: TextView)
+
+    object Initial : State {
+        private fun readResolve(): Any = Initial
+        override fun apply(linearLayout: LinearLayout, textView: TextView) = Unit
+    }
+
+    object Removed : State {
+        private fun readResolve(): Any = Removed
+        override fun apply(linearLayout: LinearLayout, textView: TextView) {
+            linearLayout.removeView(textView)
+        }
     }
 }
