71a661e14dbe1c7447f1b137e8182e04f91a7652
sardoriiiy <leo@indev.expert>
2024-07-11 16:46:30 +0500

    14 done

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,35 @@
 package ru.easycode.zerotoheroandroidtdd
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private var uiState: UiState = Init
+    private lateinit var textView: TextView
+    private lateinit var actionButton: Button
+    private lateinit var progressBar: ProgressBar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        textView = findViewById(R.id.titleTextView)
+        actionButton = findViewById(R.id.actionButton)
+        progressBar = findViewById(R.id.progressBar)
+
+        uiState.apply(textView, progressBar, actionButton)
+
+        actionButton.setOnClickListener {
+            uiState = Loading
+            uiState.apply(textView, progressBar, actionButton)
+            actionButton.postDelayed({
+                uiState = Loaded
+                uiState.apply(textView, progressBar, actionButton)
+            }, 2000)
+        }
     }
 }
---
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/UiState.kt
@@ -0,0 +1,34 @@
+package ru.easycode.zerotoheroandroidtdd
+
+import android.widget.Button
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.core.view.isVisible
+
+interface UiState{
+    fun apply(textView: TextView, progressBar: ProgressBar, button: Button)
+}
+
+object Init : UiState{
+    override fun apply(textView: TextView, progressBar: ProgressBar, button: Button) {
+        progressBar.isVisible = false
+        textView.isVisible = false
+        button.isEnabled = true
+    }
+}
+
+object Loading : UiState{
+    override fun apply(textView: TextView, progressBar: ProgressBar, button: Button) {
+        progressBar.isVisible = true
+        textView.isVisible = false
+        button.isEnabled = false
+    }
+}
+
+object Loaded : UiState{
+    override fun apply(textView: TextView, progressBar: ProgressBar, button: Button) {
+        progressBar.isVisible = false
+        textView.isVisible = true
+        button.isEnabled = true
+    }
+}
